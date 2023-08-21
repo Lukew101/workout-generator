693a507336594fcfcce9bd5dd558bc4d9a48d536
Luke Williams <lukewilliams10101@gmail.com>
2023-08-21 14:45:42 +0200

    feat: exercise model set up

---
+++ backend/src/main/java/com/example/backend/Exercise.java
@@ -0,0 +1,68 @@
+package com.example.backend;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Exercise {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+    private String type;
+    private String muscle;
+    private String difficulty;
+    private String instructions;
+
+    public Exercise(String name, String type, String muscle, String difficulty, String instructions) {
+        this.name = name;
+        this.type = type;
+        this.muscle = muscle;
+        this.difficulty = difficulty;
+        this.instructions = instructions;
+    }
+
+    public Exercise() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getMuscle() {
+        return muscle;
+    }
+
+    public void setMuscle(String muscle) {
+        this.muscle = muscle;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+}
