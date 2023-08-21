0fed0006449ba65b240e0545c51e540b2c9f2673
Luke Williams <lukewilliams10101@gmail.com>
2023-08-21 14:46:05 +0200

    feat: program model set up

--- backend/src/main/java/com/example/backend/Program.java
+++ backend/src/main/java/com/example/backend/Program.java
@@ -1,2 +1,53 @@
-package com.example.backend;public class Program {
+package com.example.backend;
+
+import jakarta.persistence.*;
+import net.minidev.json.annotate.JsonIgnore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Program {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "program", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Exercise> exercises = new ArrayList<>();
+
+    public Program(String name) {
+        this.name = name;
+    }
+
+    public Program() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Exercise> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(List<Exercise> exercises) {
+        this.exercises = exercises;
+    }
 }
