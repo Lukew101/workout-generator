77405e8f45cda903ae7f7a0112a368a7d6b3464d
Luke Williams <lukewilliams10101@gmail.com>
2023-08-21 15:26:49 +0200

    feat: basic structure for external API fetch implemented

--- backend/src/main/java/com/example/backend/ExerciseService.java
+++ backend/src/main/java/com/example/backend/ExerciseService.java
@@ -1,2 +1,25 @@
-package com.example.backend;public class ExerciseService {
+package com.example.backend;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.List;
+
+@Service
+public class ExerciseService {
+
+    public ExerciseAPIResponseDTO getExerciseList(ExerciseResponseDTO exerciseReponseDTO) {
+        WebClient webClient = WebClient.create("https://exercises-by-api-ninjas.p.rapidapi.com");
+
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/v1/exercises")
+                        .queryParam("type", exerciseReponseDTO.type())
+                        .queryParam("muscle", exerciseReponseDTO.muscle())
+                        .queryParam("difficulty", exerciseReponseDTO.difficulty())
+                        .build())
+                .retrieve()
+                .bodyToMono(ExerciseAPIResponseDTO.class)
+                .block();
+    }
 }
