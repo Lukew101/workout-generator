dff8f15006b51c5ab557303cda25a6f9530b1307
Luke Williams <lukewilliams10101@gmail.com>
2023-08-22 15:51:22 +0200

    feat: get mapping logic implemented

--- backend/src/main/java/com/example/backend/ApplicationController.java
+++ backend/src/main/java/com/example/backend/ApplicationController.java
@@ -1,35 +1,33 @@
 package com.example.backend;
 
-import com.example.backend.Exercise.ExerciseAPIResponseListDTO;
-import com.example.backend.Exercise.ExerciseResponseDTO;
-import com.example.backend.Exercise.ExerciseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "*")
-public class Controller {
+public class ApplicationController {
 
     private final ExerciseService exerciseService;
 
     @Autowired
-    public Controller(ExerciseService exerciseService) {
+    public ApplicationController(ExerciseService exerciseService) {
         this.exerciseService = exerciseService;
     }
 
     @GetMapping
-    ResponseEntity<ExerciseAPIResponseListDTO> getExerciseList(
-            @RequestParam("duration") int duration,
-            @RequestParam("type") String type,
-            @RequestParam("muscle") String muscle,
-            @RequestParam("difficulty") String difficulty
+    ResponseEntity<List<ExerciseAPIResponseDTO>> getExerciseList(
+//            @RequestParam("duration") int duration,
+//            @RequestParam("type") String type,
+//            @RequestParam("muscle") String muscle,
+//            @RequestParam("difficulty") String difficulty
     ) throws IOException, InterruptedException {
-        ExerciseResponseDTO exerciseResponseDTO = new ExerciseResponseDTO(duration, type, muscle, difficulty);
-        ExerciseAPIResponseListDTO exerciseList = exerciseService.getExerciseList(exerciseResponseDTO);
+//        ExerciseResponseDTO exerciseResponseDTO = new ExerciseResponseDTO(duration, type, muscle, difficulty);
+        List<ExerciseAPIResponseDTO> exerciseList = exerciseService.getExerciseList();
 
         return ResponseEntity.ok(exerciseList);
     }
