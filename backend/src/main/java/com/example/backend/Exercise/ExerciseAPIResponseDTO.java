b9ee296c15c8989759457124018eb9704616e520
Luke Williams <lukewilliams10101@gmail.com>
2023-08-22 13:50:39 +0200

    feat: ExerciseAPIResponseDTO added for individual exercises in API call

--- backend/src/main/java/com/example/backend/Exercise/ExerciseAPIResponseDTO.java
+++ backend/src/main/java/com/example/backend/Exercise/ExerciseAPIResponseDTO.java
@@ -1,8 +1,2 @@
-package com.example.backend.Exercise;
-
-import com.example.backend.Exercise.Exercise;
-
-import java.util.List;
-
-public record ExerciseAPIResponseDTO(List<Exercise> exerciseList) {
+package com.example.backend.Exercise;public record ExerciseAPIResponseDTO() {
 }
