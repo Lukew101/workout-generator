4df7eec929fb4c17d14dd6199798d146e1ea35e4
Luke Williams <lukewilliams10101@gmail.com>
2023-08-22 15:44:19 +0200

    feat: fetch to external api logic

--- backend/src/main/java/com/example/backend/ExerciseService.java
+++ backend/src/main/java/com/example/backend/ExerciseService.java
@@ -1,25 +1,24 @@
-package com.example.backend.Exercise;
+package com.example.backend;
 
-import com.example.backend.Exercise.ExerciseAPIResponseListDTO;
-import com.example.backend.Exercise.ExerciseResponseDTO;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 
 @Service
 public class ExerciseService {
 
-    @Value("${apiKey}")
+    @Value("${API_KEY}")
     private String apiKey;
 
-    public ExerciseAPIResponseListDTO getExerciseList(ExerciseResponseDTO exerciseReponseDTO) throws IOException, InterruptedException {
+    public List<ExerciseAPIResponseDTO> getExerciseList() throws IOException, InterruptedException {
 
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://exercises-by-api-ninjas.p.rapidapi.com/v1/exercises?type=strength&muscle=biceps&difficulty=beginner"))
@@ -28,19 +27,14 @@ public class ExerciseService {
                 .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
 
-        try {
+
             HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
             String responseBody = response.body();
 
             ObjectMapper objectMapper = new ObjectMapper();
 
-            System.out.println(objectMapper.readValue(responseBody, ExerciseAPIResponseListDTO.class));
-
-            return objectMapper.readValue(responseBody, ExerciseAPIResponseListDTO.class);
-
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-            throw e;
-        }
+            return objectMapper.readValue(
+                    responseBody,
+                    new TypeReference<List<ExerciseAPIResponseDTO>>() {});
     }
 }
