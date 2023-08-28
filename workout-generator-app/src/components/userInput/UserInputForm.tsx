6334b82378586fe0444fa9c50413b154ca84336a
Luke Williams <lukewilliams10101@gmail.com>
2023-08-28 17:11:38 +0200

    feat: extra fields added & null assigned if no value

--- workout-generator-app/src/components/userInput/UserInputForm.tsx
+++ workout-generator-app/src/components/userInput/UserInputForm.tsx
@@ -10,16 +10,23 @@ const UserInputForm = ({ setExercises }: AddEntityFormProps) => {
     event.preventDefault();
 
     const formData = new FormData();
+    const formElement = event.target as HTMLFormElement;
 
-    const duration = (event.target as HTMLFormElement).duration.value;
-    const trainingType = (event.target as HTMLFormElement).type.value;
-    const muscle = (event.target as HTMLFormElement).muscle.value;
-    const difficulty = (event.target as HTMLFormElement).difficulty.value;
-
+    const duration = formElement.duration.value;
     formData.append("duration", duration);
+
+    const trainingType = formElement.type.value;
     formData.append("type", trainingType);
+
+    const muscleField = formElement.muscle;
+    const muscle = muscleField ? muscleField.value : null;
     formData.append("muscle", muscle);
+
+    const difficultyField = formElement.difficulty;
+    const difficulty = difficultyField ? difficultyField.value : null;
     formData.append("difficulty", difficulty);
+
+    console.log(formData);
 
     try {
       const response = await fetch("http://localhost:8080/api/exercises", {
@@ -41,57 +48,190 @@ const UserInputForm = ({ setExercises }: AddEntityFormProps) => {
   };
 
   return (
-    <div>
-      <h2>Generate Exercises</h2>
-      <form onSubmit={handleFormSubmit} id="programFormInput">
-        <label>Session Duration</label>
-        <select name="duration" defaultValue="" id="trainingDuration">
-          <option value="15">15 minutes</option>
-          <option value="30">30 minutes</option>
-          <option value="45">45 minutes</option>
-          <option value="60+">60+ minutes</option>
-        </select>
-        <label>Training type</label>
-        <select name="type" defaultValue="" id="trainingType">
-          <option value="strength">Strength</option>
-          {/*
-          <option value="cardio">Cardio</option>
-          <option value="powerLifting">Power Lifting</option>
-          <option value="strongman">Strongman</option>
-          <option value="plyometrics">Plyometrics</option>
-          <option value="stretching">Stretching</option>
-          */}
-        </select>
-        <label>Muscle</label>
-        <select name="muscle" defaultValue="" id="trainingMuscle">
-          <option value="abdominals">Abdominals</option>
-          <option value="abductors">Abductors</option>
-          <option value="biceps">Biceps</option>
-          <option value="calves">Calves</option>
-          <option value="Chest">Chest</option>
-          <option value="forearms">Forearms</option>
-          <option value="glutes">Glutes</option>
-          <option value="hamstrings">Hamstrings</option>
-          <option value="lats">Lats</option>
-          <option value="lowerBack">Lower Back</option>
-          <option value="middleBack">Middle Back</option>
-          <option value="neck">Neck</option>
-          <option value="quadriceps">Quadriceps</option>
-          <option value="traps">Traps</option>
-          <option value="triceps">Triceps</option>
-        </select>
-        <label>Difficulty</label>
-        <select name="difficulty" defaultValue="" id="trainingType">
-          <option value="beginner">Beginner (0-3 months)</option>
-          <option value="intermediate">Intermediate (4-12 months)</option>
-          <option value="expert">Expert (12+ months)</option>
-        </select>
-        <button type="submit" className="form__button">
-          Generate
-        </button>
-      </form>
-    </div>
+    <>
+        <div>
+            <h2>Strength - muscle 1</h2>
+            <form onSubmit={handleFormSubmit} id="muscle1FormInput">
+                <label>Session Duration</label>
+                <select name="duration" defaultValue="" id="trainingDuration">
+                    <option value="0">Null</option>
+                    <option value="15">15 minutes</option>
+                    <option value="30">30 minutes</option>
+                    <option value="45">45 minutes</option>
+                    <option value="60+">60+ minutes</option>
+                </select>
+                <label>Training type</label>
+                <select name="type" defaultValue="" id="trainingType">
+                    <option value="strength">Strength</option>
+                </select>
+                <label>Muscle</label>
+                <select name="muscle" defaultValue="" id="trainingMuscle">
+                    <option value="abdominals">Abdominals</option>
+                    <option value="abductors">Abductors</option>
+                    <option value="biceps">Biceps</option>
+                    <option value="calves">Calves</option>
+                    <option value="Chest">Chest</option>
+                    <option value="forearms">Forearms</option>
+                    <option value="glutes">Glutes</option>
+                    <option value="hamstrings">Hamstrings</option>
+                    <option value="lats">Lats</option>
+                    <option value="lower_back">Lower Back</option>
+                    <option value="middle_back">Middle Back</option>
+                    <option value="neck">Neck</option>
+                    <option value="quadriceps">Quadriceps</option>
+                    <option value="traps">Traps</option>
+                    <option value="triceps">Triceps</option>
+                </select>
+                <label>Difficulty</label>
+                <select name="difficulty" defaultValue="" id="trainingType">
+                    <option value="beginner">Beginner (0-3 months)</option>
+                    <option value="intermediate">Intermediate (4-12 months)</option>
+                    <option value="expert">Expert (12+ months)</option>
+                </select>
+                <button type="submit" className="form__button">
+                Generate
+                </button>
+            </form> 
+        </div>
+        <div>
+            <h2>Strength - muscle 2</h2>
+            <form onSubmit={handleFormSubmit} id="muscle2FormInput">
+                <label>Session Duration</label>
+                <select name="duration" defaultValue="" id="trainingDuration">
+                    <option value="0">Null</option>
+                    <option value="15">15 minutes</option>
+                    <option value="30">30 minutes</option>
+                    <option value="45">45 minutes</option>
+                    <option value="60+">60+ minutes</option>
+                </select>
+                <label>Training type</label>
+                <select name="type" defaultValue="" id="trainingType">
+                    <option value="strength">Strength</option>
+                </select>
+                <label>Muscle</label>
+                <select name="muscle" defaultValue="" id="trainingMuscle">
+                    <option value="abdominals">Abdominals</option>
+                    <option value="abductors">Abductors</option>
+                    <option value="biceps">Biceps</option>
+                    <option value="calves">Calves</option>
+                    <option value="Chest">Chest</option>
+                    <option value="forearms">Forearms</option>
+                    <option value="glutes">Glutes</option>
+                    <option value="hamstrings">Hamstrings</option>
+                    <option value="lats">Lats</option>
+                    <option value="lower_back">Lower Back</option>
+                    <option value="middle_back">Middle Back</option>
+                    <option value="neck">Neck</option>
+                    <option value="quadriceps">Quadriceps</option>
+                    <option value="traps">Traps</option>
+                    <option value="triceps">Triceps</option>
+                </select>
+                <label>Difficulty</label>
+                <select name="difficulty" defaultValue="" id="trainingType">
+                    <option value="beginner">Beginner (0-3 months)</option>
+                    <option value="intermediate">Intermediate (4-12 months)</option>
+                    <option value="expert">Expert (12+ months)</option>
+                </select>
+                <button type="submit" className="form__button">
+                Generate
+                </button>
+            </form> 
+        </div>
+        <div>
+            <h2>Cardio</h2>
+            <form onSubmit={handleFormSubmit} id="muscle1FormInput">
+                <label>Session Duration</label>
+                <select name="duration" defaultValue="" id="trainingDuration">
+                    <option value="0">Null</option>
+                    <option value="15">15 minutes</option>
+                    <option value="30">30 minutes</option>
+                    <option value="45">45 minutes</option>
+                    <option value="60+">60+ minutes</option>
+                </select>
+                <label>Training type</label>
+                <select name="type" defaultValue="" id="trainingType">
+                    <option value="cardio">Cardio</option>
+                </select>
+                <label>Difficulty</label>
+                <select name="difficulty" defaultValue="" id="trainingType">
+                    <option value="beginner">Beginner</option>
+                    <option value="intermediate">Intermediate</option>
+                </select>
+                <button type="submit" className="form__button">
+                Generate
+                </button>
+            </form> 
+        </div>
+        <div>
+            <h2>Plyometrics</h2>
+            <form onSubmit={handleFormSubmit} id="plyometrics">
+                <label>Session Duration</label>
+                <select name="duration" defaultValue="" id="trainingDuration">
+                    <option value="0">Null</option>
+                    <option value="15">15 minutes</option>
+                    <option value="30">30 minutes</option>
+                    <option value="45">45 minutes</option>
+                    <option value="60+">60+ minutes</option>
+                </select>
+                <label>Training type</label>
+                <select name="type" defaultValue="" id="trainingType">
+                    <option value="plyometrics">Plyometrics</option>
+                </select>
+                <label>Difficulty</label>
+                <select name="difficulty" defaultValue="" id="trainingType">
+                    <option value="beginner">Beginner (0-3 months)</option>
+                    <option value="intermediate">Intermediate (4-12 months)</option>
+                    <option value="expert">Expert (12+ months)</option>
+                </select>
+                <button type="submit" className="form__button">
+                Generate
+                </button>
+            </form> 
+        </div>
+        <div>
+            <h2>Stretching</h2>
+            <form onSubmit={handleFormSubmit} id="stretching">
+                <label>Session Duration</label>
+                <select name="duration" defaultValue="" id="trainingDuration">
+                    <option value="0">Null</option>
+                    <option value="15">15 minutes</option>
+                    <option value="30">30 minutes</option>
+                    <option value="45">45 minutes</option>
+                    <option value="60+">60+ minutes</option>
+                </select>
+                <label>Training type</label>
+                <select name="type" defaultValue="" id="trainingType">
+                <option value="stretching">Stretching</option>
+                </select>
+                <label>Muscle</label>
+                <select name="muscle" defaultValue="" id="trainingMuscle">
+                    <option value="abdominals">Abdominals</option>
+                    <option value="abductors">Abductors</option>
+                    <option value="biceps">Biceps</option>
+                    <option value="calves">Calves</option>
+                    <option value="Chest">Chest</option>
+                    <option value="forearms">Forearms</option>
+                    <option value="glutes">Glutes</option>
+                    <option value="hamstrings">Hamstrings</option>
+                    <option value="lats">Lats</option>
+                    <option value="lower_back">Lower Back</option>
+                    <option value="middle_back">Middle Back</option>
+                    <option value="neck">Neck</option>
+                    <option value="quadriceps">Quadriceps</option>
+                    <option value="traps">Traps</option>
+                    <option value="triceps">Triceps</option>
+                </select>
+                <button type="submit" className="form__button">
+                Generate
+                </button>
+            </form> 
+        </div>
+    </> 
   );
 };
 
 export default UserInputForm;
+
+// Make it so there are drop down menus for each training type (couple for strength)
+// The user then puts in how long they will train on each. The time will
+// determine how many cards they can select.
