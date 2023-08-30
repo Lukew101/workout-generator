e455d9ea516866602f7cdab38714be6fdc1cea16
Luke Williams <lukewilliams10101@gmail.com>
2023-08-30 13:34:01 +0200

    refactor: refactor app to take seperate components

--- workout-generator-app/src/App.tsx
+++ workout-generator-app/src/App.tsx
@@ -1,21 +1,35 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import { useState } from 'react'
 import './App.css'
-import UserInputForm from './components/userInput/UserInputForm'
 import ExcerciseBoard from './components/exerciseBoard/ExcerciseBoard'
+import StrengthTrainingInputForm from './components/userInput/StrengthTrainingInputForm';
+import CardioInputForm from './components/userInput/CardioTrainingInputForm';
+import PlyometricsInputForm from './components/userInput/PlyometricsInputForm';
+import StretchingInputForm from './components/userInput/StretchingInputForm';
 
 
 function App() {
-  const [exercises, setExercises] = useState<Exercise[]>([])
+  const [stengthExercisesOne, setStrengthExercisesOne] = useState<Exercise[]>([]);
+  const [stengthExercisesTwo, setStrengthExercisesTwo] = useState<Exercise[]>([]);
+  const [cardioExercises, setCardioExercises] = useState<Exercise[]>([]);
+  const [plyometricExercises, setPlyometricExercises] = useState<Exercise[]>([]);
+  const [stretchingExercises, setStretchingExercises] = useState<Exercise[]>([]);
 
   return (
     <main>
       <h1>The Workout Generator</h1>
       <h2>How to use?</h2>
-      <p>Fill out the inputs below, click generate to get a list of exercise cards, pick the number of specified cards, perform the exercises and save the program if you like it.
-      </p>
-      <UserInputForm setExercises={setExercises} />
-      <ExcerciseBoard exercises={exercises}/>
+      <p>Fill out the inputs below, click generate to get a list of exercise cards, pick the number of specified cards, perform the exercises and save the program if you like it.</p>
+      <StrengthTrainingInputForm setExercises={setStrengthExercisesOne} />
+      <ExcerciseBoard exercises={stengthExercisesOne} />
+      <StrengthTrainingInputForm setExercises={setStrengthExercisesTwo} />
+      <ExcerciseBoard exercises={stengthExercisesTwo} />
+      <CardioInputForm setExercises={setCardioExercises} />
+      <ExcerciseBoard exercises={cardioExercises} />
+      <PlyometricsInputForm setExercises={setPlyometricExercises} />
+      <ExcerciseBoard exercises={plyometricExercises} />
+      <StretchingInputForm setExercises={setStretchingExercises} />
+      <ExcerciseBoard exercises={stretchingExercises} />
     </main>
   )
 }
