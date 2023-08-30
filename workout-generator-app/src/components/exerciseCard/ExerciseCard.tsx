126578ede50734eb9b06d146e69a29757988815d
Luke Williams <lukewilliams10101@gmail.com>
2023-08-30 15:45:01 +0200

    feat: rotating exercise cards

--- workout-generator-app/src/components/exerciseCard/ExerciseCard.tsx
+++ workout-generator-app/src/components/exerciseCard/ExerciseCard.tsx
@@ -1,35 +1,54 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import './exerciseCard.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "./exerciseCard.css";
+import { useState } from "react";
 
 type ExerciseProps = {
   exercise: Exercise;
 };
 
 const ExerciseCard = ({ exercise }: ExerciseProps) => {
+  const [isFlipped, setIsFlipped] = useState(false);
 
+  const toggleFlipped = () => {
+    setIsFlipped(!isFlipped);
+  };
 
   return (
-    <div className='card'>
-      <div className='card-body'>
-        <h2 className='card-title card-title-multi-line'>{exercise.name}</h2>
-        <p className='card-text instructions-style'>{exercise.instructions}</p>
+    <div
+      className={`card-container ${isFlipped ? "flipped" : ""}`}
+      onClick={toggleFlipped}
+    >
+      <div className="card-inner">
+        <div className="card-front">
+          <p className="click-to-flip">Click to view exercise</p>
+        </div>
+        <div className="card-back">
+          <div>
+            <h2 className="card-title card-title-multi-line">
+              {exercise.name}
+            </h2>
+            <p className="instructions-style">{exercise.instructions}</p>
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6">
+                <h3 className="card-subtitle">Equipment</h3>
+                <p>{exercise.equipment}</p>
+              </div>
+              <div className="col-md-6">
+                <h3 className="card-subtitle">Difficulty</h3>
+                <p>{exercise.difficulty}</p>
+              </div>
+              <div className="col-md-6">
+                <h3 className="card-subtitle">Muscle/s</h3>
+                <p>{exercise.muscle}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <ul className='list-group list-group-flush'>
-        <li className='list-group-item'>
-          <h3>Equipment</h3>
-          <p>{exercise.equipment}</p>
-        </li>
-        <li className='list-group-item'>
-          <h3>Difficulty</h3>
-          <p>{exercise.difficulty}</p>
-        </li>
-        <li className='list-group-item'>
-          <h3>Muscle/s</h3>
-          <p>{exercise.muscle}</p>
-        </li>
-      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default ExerciseCard
+export default ExerciseCard;
