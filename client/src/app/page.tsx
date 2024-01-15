2e64750df974904561fee9c09f49a8aa8d9020aa
Luke Williams <lukewilliams10101@gmail.com>
2024-01-15 11:56:20 +0100

    feat: next.js implemented for frontend

---
+++ client/src/app/page.tsx
@@ -0,0 +1,97 @@
+import Image from "next/image";
+
+export default function Home() {
+  const [selectedExerciseType, setSelectedExerciseType] = useState("");
+  const [exercises, setExercises] = useState<{
+    [key: string]: Exercise[];
+  }>({});
+
+  const handleExerciseTypeChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSelectedExerciseType(event.target.value);
+  };
+
+  const handleGenerateExerciseSet = (exercises: Exercise[]) => {
+    setExercises((prevExercises) => ({
+      ...prevExercises,
+      [selectedExerciseType]: [
+        ...(prevExercises[selectedExerciseType] || []),
+        ...exercises,
+      ],
+    }));
+  };
+
+  const renderExerciseFormAndBoard = () => {
+    if (selectedExerciseType) {
+      switch (selectedExerciseType) {
+        case "strength":
+          return (
+            <>
+              <StrengthTrainingInputForm
+                setExercises={handleGenerateExerciseSet}
+              />
+              <ExcerciseBoard
+                exercises={exercises[selectedExerciseType] || []}
+                formTitle="Strength Exercises"
+              />
+            </>
+          );
+        case "cardio":
+          return (
+            <>
+              <CardioInputForm setExercises={handleGenerateExerciseSet} />
+              <ExcerciseBoard
+                exercises={exercises[selectedExerciseType] || []}
+                formTitle="Cardiovascular Exercises"
+              />
+            </>
+          );
+        case "plyometrics":
+          return (
+            <>
+              <PlyometricsInputForm setExercises={handleGenerateExerciseSet} />
+              <ExcerciseBoard
+                exercises={exercises[selectedExerciseType] || []}
+                formTitle="Plyometric Exercises"
+              />
+            </>
+          );
+        case "stretching":
+          return (
+            <>
+              <StretchingInputForm setExercises={handleGenerateExerciseSet} />
+              <ExcerciseBoard
+                exercises={exercises[selectedExerciseType] || []}
+                formTitle="Stretching Exercises"
+              />
+            </>
+          );
+        default:
+          return null;
+      }
+    }
+    return null;
+  };
+
+  return (
+    <main>
+      <h1>The Workout Generator</h1>
+      <h2>How to use?</h2>
+      <p>
+        Fill out the inputs below, click generate to get a list of exercise
+        cards for each exercise type, flip the cards, save the program and save
+        the program if you like it.
+      </p>
+      <label>Select Exercise Type:</label>
+      <select value={selectedExerciseType} onChange={handleExerciseTypeChange}>
+        <option value="">Select an exercise type</option>
+        <option value="strength">Strength Training</option>
+        <option value="cardio">Cardiovascular</option>
+        <option value="plyometrics">Plyometrics</option>
+        <option value="stretching">Stretching</option>
+      </select>
+      {renderExerciseFormAndBoard()}
+    </main>
+  );
+}
