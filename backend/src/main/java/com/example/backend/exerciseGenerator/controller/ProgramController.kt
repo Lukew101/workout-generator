f28878b4673de83fedd0e84519aef9682c064025
Luke Williams <lukewilliams10101@gmail.com>
2024-01-15 16:55:45 +0100

    feat: implemented ProgramController

--- backend/src/main/java/com/example/backend/exerciseGenerator/controller/ProgramController.kt
+++ backend/src/main/java/com/example/backend/exerciseGenerator/controller/ProgramController.kt
@@ -1,11 +1,12 @@
 package com.example.backend.exerciseGenerator.controller
 
 import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/program")
 @CrossOrigin(origins = ["*"])
 class ProgramController {
 
