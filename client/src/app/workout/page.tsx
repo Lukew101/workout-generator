4bbaaef255c634a22c477df517e239cb492c1bd1
Luke Williams <lukewilliams10101@gmail.com>
2024-01-21 20:15:40 +0100

    feat: workout page seperated from landing page

---
+++ client/src/app/workout/page.tsx
@@ -0,0 +1,104 @@
+"use client";
+import { useState } from "react";
+import StrengthTrainingInputForm from "../components/userInput/StrengthTrainingInput";
+import ExcerciseBoard from "../components/ExerciseBoard";
+import CardioInputForm from "../components/userInput/CardioTrainingInput";
+import PlyometricsInputForm from "../components/userInput/PlyometricsInput";
+import StretchingInputForm from "../components/userInput/StretchingInput";
+
+export default function Workout() {
+  const [selectedExerciseType, setSelectedExerciseType] = useState("");
+  const [exercises, setExercises] = useState<{
+    [key: string]: Exercise[];
+  }>({});
+
+  const handleExerciseTypeChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setSelectedExerciseType(event.target.value);
+  };
+
+  const handleGenerateExerciseSet = (exercises: Exercise[]) => {
+    setExercises((prevExercises) => ({
+      ...prevExercises,
+      [selectedExerciseType]: [
+        ...(prevExercises[selectedExerciseType] || []),
+        ...exercises,
+      ],
+    }));
+  };
+
+  const renderExerciseFormAndBoard = () => {
+    if (selectedExerciseType) {
+      const selectedExerciseInputForm = () => {
+        switch (selectedExerciseType) {
+          case "strength":
+            return (
+              <StrengthTrainingInputForm
+                setExercises={handleGenerateExerciseSet}
+              />
+            );
+          case "cardio":
+            return <CardioInputForm setExercises={handleGenerateExerciseSet} />;
+          case "plyometrics":
+            return (
+              <PlyometricsInputForm setExercises={handleGenerateExerciseSet} />
+            );
+          case "stretching":
+            return (
+              <StretchingInputForm setExercises={handleGenerateExerciseSet} />
+            );
+          default:
+            return null;
+        }
+      };
+
+      return (
+        <>
+          {selectedExerciseInputForm()}
+          {exercises["strength"] && (
+            <ExcerciseBoard
+              exercises={exercises["strength"] || []}
+              formTitle="Strength Exercises"
+            />
+          )}
+          {exercises["cardio"] && (
+            <ExcerciseBoard
+              exercises={exercises["cardio"] || []}
+              formTitle="Cardiovascular Exercises"
+            />
+          )}
+          {exercises["plyometrics"] && (
+            <ExcerciseBoard
+              exercises={exercises["plyometrics"] || []}
+              formTitle="Plyometric Exercises"
+            />
+          )}
+          {exercises["stretching"] && (
+            <ExcerciseBoard
+              exercises={exercises["stretching"] || []}
+              formTitle="Stretching Exercises"
+            />
+          )}
+        </>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <main>
+      <h1>The Workout Generator</h1>
+      <h2>Create your workout</h2>
+      <label>Select Exercise Type:</label>
+      <select value={selectedExerciseType} onChange={handleExerciseTypeChange}>
+        <option value="">Select an exercise type</option>
+        <option value="strength">Strength Training</option>
+        <option value="cardio">Cardiovascular</option>
+        <option value="plyometrics">Plyometrics</option>
+        <option value="stretching">Stretching</option>
+      </select>
+      {renderExerciseFormAndBoard()}
+    </main>
+  );
+}
