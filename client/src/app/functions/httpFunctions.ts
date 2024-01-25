6df755a92ad13fc28db8c041706dba2ee79adda4
Luke Williams <lukewilliams10101@gmail.com>
2024-01-25 17:30:37 +0100

    fix: fixed function headers

--- client/src/app/functions/httpFunctions.ts
+++ client/src/app/functions/httpFunctions.ts
@@ -1,10 +1,13 @@
 export async function postForm(formData: any, setExercises: (exercises: Exercise[]) => void) {
+    console.log(formData);
     try {
         const response = await fetch("http://localhost:8080/api/exercise/exerciseList", {
             method: "POST",
             body: JSON.stringify(formData),
+            headers: {
+                'Content-Type': 'application/json',
+            },
         });
-
         if (!response.ok) {
             throw new Error("Failed to add generate exercises");
         }
