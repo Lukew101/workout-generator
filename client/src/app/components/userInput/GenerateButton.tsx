d1cdff9b31d6cc432cf9f269f82628b2dcefcdf8
Luke Williams <lukewilliams10101@gmail.com>
2024-01-25 22:57:36 +0100

    style: color changes to button

--- client/src/app/components/userInput/GenerateButton.tsx
+++ client/src/app/components/userInput/GenerateButton.tsx
@@ -7,8 +7,7 @@ interface GenerateButtonProps {
 export default function GenerateButton({ handleFormSubmit }: GenerateButtonProps){
   return (
     <button
-      type="button"
-      className="w-4/5 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+    className="w-4/5 text-white bg-accent hover:bg-accentDark focus:ring-4 focus:ring-accentLight font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mt-2"
       onClick={handleFormSubmit}
     >
       Generate
