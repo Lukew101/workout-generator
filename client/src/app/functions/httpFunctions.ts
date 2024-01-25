990519db28eda473344276eae0f9a683f25d8837
Luke Williams <lukewilliams10101@gmail.com>
2024-01-25 17:05:36 +0100

    fix: updated function for new inputs

--- client/src/app/functions/httpFunctions.ts
+++ client/src/app/functions/httpFunctions.ts
@@ -1,17 +1,15 @@
-export async function postForm(formData: FormData, setExercises: (exercises: Exercise[]) => void) {
+export async function postForm(formData: any, setExercises: (exercises: Exercise[]) => void) {
     try {
         const response = await fetch("http://localhost:8080/api/exercise/exerciseList", {
             method: "POST",
-            body: formData,
+            body: JSON.stringify(formData),
         });
 
         if (!response.ok) {
             throw new Error("Failed to add generate exercises");
         }
 
-        const data = await response.json();
-        const exerciseList = await data;
-        console.log(exerciseList);
+        const exerciseList = await response.json();
         setExercises(exerciseList);
     } catch (error) {
         console.error("Error while submitting the form:", error);
