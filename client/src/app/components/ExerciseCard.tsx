1d04a19904bed170c63268ea9e6d17fbc5965180
Luke Williams <lukewilliams10101@gmail.com>
2024-01-26 13:59:46 +0100

    style: card styling reintroduced

--- client/src/app/components/ExerciseCard.tsx
+++ client/src/app/components/ExerciseCard.tsx
@@ -5,23 +5,47 @@ type ExerciseProps = {
 };
 
 const ExerciseCard = ({ exercise }: ExerciseProps) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const toggleFlipped = () => {
+    setIsFlipped(!isFlipped);
+  };
+  
   return (
-    <>
-      <h2>{exercise.name}</h2>
-      <p>{exercise.instructions}</p>
-      <div>
-        <h3>Equipment</h3>
-        <p>{exercise.equipment}</p>
+    <div
+      className={`card-container ${isFlipped ? "flipped" : ""}`}
+      onClick={toggleFlipped}
+    >
+      <div className="card-inner">
+        <div className="card-front">
+          <p className="click-to-flip">Click to view exercise</p>
+        </div>
+        <div className="card-back">
+          <div>
+            <h2 className="card-title card-title-multi-line">
+              {exercise.name}
+            </h2>
+            <p className="instructions-style">{exercise.instructions}</p>
+          </div>
+          <div>
+            <div>
+              <div>
+                <h3 className="card-subtitle">Equipment</h3>
+                <p>{exercise.equipment}</p>
+              </div>
+              <div>
+                <h3 className="card-subtitle">Difficulty</h3>
+                <p>{exercise.difficulty}</p>
+              </div>
+              <div>
+                <h3 className="card-subtitle">Muscle/s</h3>
+                <p>{exercise.muscle}</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="col-md-6">
-        <h3 className="card-subtitle">Difficulty</h3>
-        <p>{exercise.difficulty}</p>
-      </div>
-      <div className="col-md-6">
-        <h3 className="card-subtitle">Muscle/s</h3>
-        <p>{exercise.muscle}</p>
-      </div>
-    </>
+    </div>
   );
 };
 
