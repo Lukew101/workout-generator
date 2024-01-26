fee236b103901a941648c17651f1f0ebc79515eb
Luke Williams <lukewilliams10101@gmail.com>
2024-01-26 14:46:58 +0100

    style: improved layout

--- client/src/app/components/userInput/GenerateButton.tsx
+++ client/src/app/components/userInput/GenerateButton.tsx
@@ -7,7 +7,7 @@ interface GenerateButtonProps {
 export default function GenerateButton({ handleFormSubmit }: GenerateButtonProps){
   return (
     <button
-    className="w-4/5 text-white bg-accent hover:bg-accentDark focus:ring-4 focus:ring-accentLight font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mt-2"
+    className="w-4/5 text-white bg-accent hover:bg-accentDark focus:ring-4 focus:ring-accentLight font-medium rounded-lg text-sm px-5 py-2.5 me-2 mt-2"
       onClick={handleFormSubmit}
     >
       Generate
