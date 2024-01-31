964daff5964fe1351686d6c4b8df3407e4465e95
Luke Williams <lukewilliams10101@gmail.com>
2024-01-31 23:06:24 +0100

    feat: endpoint returns auth users email

--- backend/src/main/java/com/example/backend/user/controller/UserController.kt
+++ backend/src/main/java/com/example/backend/user/controller/UserController.kt
@@ -1,4 +1,20 @@
 package com.example.backend.user.controller
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.core.oidc.user.OidcUser
+import org.springframework.web.bind.annotation.CrossOrigin
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+@RequestMapping("api/user")
 class UserController {
+    @GetMapping
+    fun getUser(@AuthenticationPrincipal user: OidcUser?): String {
+        if (user == null) return "No user"
+        return user.email.toString()
+    }
 }
