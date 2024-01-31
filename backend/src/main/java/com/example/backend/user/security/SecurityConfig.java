b9199c03db54f04fdc0ef3ba884882b4b647b9f5
Luke Williams <lukewilliams10101@gmail.com>
2024-01-31 23:08:03 +0100

    feat: implemented auth to backend

--- backend/src/main/java/com/example/backend/user/security/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/user/security/SecurityConfig.java
@@ -1,17 +1,31 @@
 package com.example.backend.user.security;
 
+import static com.example.backend.user.security.CorsConfig.withWorkoutCreativeDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import java.io.IOException;
+
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+    @Value("${okta.oauth2.issuer}")
+    private String issuer;
+    @Value("${okta.oauth2.client-id}")
+    private String clientId;
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -20,8 +34,24 @@ public class SecurityConfig {
                         .requestMatchers(antMatcher("/api/exercise/exerciseList")).permitAll()
                         .anyRequest().authenticated()
                 )
-                .oauth2Login(withDefaults());
+                .cors(withWorkoutCreativeDefaults("http://localhost:3000"))
+                .csrf(CsrfConfigurer::disable)
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler(customAuthenticationSuccessHandler))
+                .logout(logout -> logout
+                        .addLogoutHandler(logoutHandler()));
         return http.build();
+    }
+
+    private LogoutHandler logoutHandler() {
+        return (request, response, authentication) -> {
+            try {
+                String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+                response.sendRedirect(issuer + "v2/logout?client_id=" + clientId + "&returnTo=" + baseUrl);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
     }
 }
 
