67e0678a5d100df85ebd1eceb79c644ab8bb457c
Luke Williams <lukewilliams10101@gmail.com>
2024-01-31 19:45:10 +0100

    feat: auth0 implemented in backend

--- backend/src/main/java/com/example/backend/user/security/SecurityConfig.java
+++ backend/src/main/java/com/example/backend/user/security/SecurityConfig.java
@@ -5,15 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 
 @Configuration
 @EnableWebSecurity
@@ -21,27 +15,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        return http
+        http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(antMatcher("/api/exercise/exerciseList"), antMatcher("/api/status")).permitAll()
+                        .requestMatchers(antMatcher("/api/exercise/exerciseList")).permitAll()
+                        .anyRequest().authenticated()
                 )
-                .cors(withDefaults())
-                .oauth2ResourceServer(auth ->
-                        auth.jwt(withDefaults())
-                )
-                .csrf(CsrfConfigurer::disable)
-                .build();
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("*"));
-        configuration.setAllowedHeaders(List.of("*"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+                .oauth2Login(withDefaults());
+        return http.build();
     }
 }
 
