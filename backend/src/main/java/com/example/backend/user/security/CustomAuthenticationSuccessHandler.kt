f3e41c154245df530a58be688f2885f53db1984a
Luke Williams <lukewilliams10101@gmail.com>
2024-01-31 23:04:07 +0100

    feat: implemented cors config

---
+++ backend/src/main/java/com/example/backend/user/security/CustomAuthenticationSuccessHandler.kt
@@ -0,0 +1,33 @@
+package com.example.backend.user.security
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.Authentication
+import org.springframework.security.oauth2.core.oidc.user.OidcUser
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler
+import org.springframework.stereotype.Component
+import java.io.IOException
+
+@Component
+class CustomAuthenticationSuccessHandler : AuthenticationSuccessHandler {
+    @Throws(IOException::class)
+    override fun onAuthenticationSuccess(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authentication: Authentication
+    ) {
+        val oidcUser = authentication.principal as OidcUser
+        response.setHeader("Access-Control-Allow-Credentials", "true")
+
+//        response.addCookie(createNewCookie(oidcUser.getIdToken().getTokenValue()));
+        response.sendRedirect("http://localhost:3000/")
+    } //    private Cookie createNewCookie(String tokenValue) {
+    //        Cookie cookie = new Cookie("JwtToken", tokenValue);
+    //        cookie.setHttpOnly(true);
+    //        cookie.setMaxAge(3500);
+    //        cookie.setSecure(true);
+    //        cookie.setPath("/");
+    //        cookie.setDomain(getDomain(websiteProperties.frontend()));
+    //        return cookie;
+    //    }
+}
