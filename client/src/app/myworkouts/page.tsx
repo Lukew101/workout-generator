e471c079afac11c71d463f9d217ef7d2694199c6
Luke Williams <lukewilliams10101@gmail.com>
2024-02-01 15:22:55 +0100

    feat: user fetched on frontend

--- client/src/app/myworkouts/page.tsx
+++ client/src/app/myworkouts/page.tsx
@@ -1,18 +1,23 @@
 "use client";
-
 import { useEffect, useState } from "react";
+
+type User = {
+  name: string;
+  email: string;
+  profilePicture: string;
+};
 
 
 export default function ProfileClient() {
-  const [email, setEmail] = useState<string>("");
+  const [user, setUser] = useState<User>();
   const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
   const fetchAuthStatus= async () => {
     const res = await fetch(`${BACKEND_URL}/user`, {
       credentials: "include",
     });
-    const data = await res.text();
-    setEmail(data);
+    const data = await res.json();
+    setUser(data);
     console.log(data);
   }
 
@@ -22,9 +27,11 @@ export default function ProfileClient() {
 
 
   return (
-    email && (
+    user && (
       <div>
-        <h2 className="mt-20">{email}</h2>
+        <h2 className="mt-20">{user.name}</h2>
+        <h2>{user.email}</h2>
+        <img src={user.profilePicture} alt={`${user.name} profile picture`} />
       </div>
     )
   );
