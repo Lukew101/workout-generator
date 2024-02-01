ae659007ac1fcad6e2c0bc7f6a926bbabbc6b5b7
Luke Williams <lukewilliams10101@gmail.com>
2024-02-01 21:42:07 +0100

    feat: creatProgram implemented

--- backend/src/main/java/com/example/backend/user/service/UserService.kt
+++ backend/src/main/java/com/example/backend/user/service/UserService.kt
@@ -1,7 +1,10 @@
 package com.example.backend.user.service
 
 import com.example.backend.exceptionHandler.exception.UserNotFoundException
+import com.example.backend.exercise_generator.model.Exercise
+import com.example.backend.user.controller.dtos.ProgramRequestDTO
 import com.example.backend.user.controller.dtos.UserResponseData
+import com.example.backend.user.model.Program
 import org.springframework.security.access.AccessDeniedException
 import org.springframework.security.oauth2.core.oidc.user.OidcUser
 import org.springframework.stereotype.Service
@@ -21,5 +24,19 @@ class UserService(
             email = workoutCreativeUser.email,
             profilePicture = workoutCreativeUser.profilePicture
         )
+    }
+
+    fun createProgram(programData: ProgramRequestDTO, user: OidcUser): Program {
+        val program = Program()
+        program.name = programData.name
+
+        programData.exerciseList.forEach { exercise ->
+            program.exercises.add(exercise)
+        }
+
+        program.user = userRepository.findByEmail(user.email)
+            ?: throw UserNotFoundException("User not found")
+
+        return program
     }
 }
