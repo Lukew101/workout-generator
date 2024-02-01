1b892bd358d6b52d2c39f3481fa7166df19f46d0
Luke Williams <lukewilliams10101@gmail.com>
2024-02-01 21:54:58 +0100

    feat: implemented test .yml

---
+++ backend/src/test/java/com/example/backend/status/StatusControllerTest.kt
@@ -0,0 +1,12 @@
+package com.example.backend.status
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class StatusControllerTest {
+
+    @Test
+    fun statusCheck() {
+    }
+}
