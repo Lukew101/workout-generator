9cee78abef46c2084e3735317ce8350b0dff7734
Luke Williams <lukewilliams10101@gmail.com>
2024-02-01 15:22:16 +0100

    feat: user not found exception implemented

--- backend/src/main/java/com/example/backend/exceptionHandler/exception/UserNotFoundException.java
+++ backend/src/main/java/com/example/backend/exceptionHandler/exception/UserNotFoundException.java
@@ -1,2 +1,7 @@
-package com.example.backend.exceptionHandler.exception;public class UserNotFoundException {
+package com.example.backend.exceptionHandler.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
