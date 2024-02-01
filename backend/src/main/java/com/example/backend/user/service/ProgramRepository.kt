17c1737856994271cf5050825b7dca92f10b28a7
Luke Williams <lukewilliams10101@gmail.com>
2024-02-01 21:42:35 +0100

    feat: ProgramRepository implemented

--- backend/src/main/java/com/example/backend/user/service/ProgramRepository.kt
+++ backend/src/main/java/com/example/backend/user/service/ProgramRepository.kt
@@ -1,4 +1,8 @@
 package com.example.backend.user.service
 
-interface ProgramRepository {
-}
+import com.example.backend.user.model.Program
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ProgramRepository: JpaRepository<Program, Long>
