9ba8b17e93315044b4e836929b6ace905e314261
Luke Williams <lukewilliams10101@gmail.com>
2024-02-01 15:39:31 +0100

    feat: user created if does not exist in db

--- backend/src/main/java/com/example/backend/user/security/CustomAuthenticationSuccessHandler.kt
+++ backend/src/main/java/com/example/backend/user/security/CustomAuthenticationSuccessHandler.kt
@@ -1,7 +1,11 @@
 package com.example.backend.user.security
 
+import com.example.backend.user.model.WorkoutCreativeUser
+import com.example.backend.user.service.UserRepository
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.hibernate.jdbc.Work
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.core.oidc.user.OidcUser
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler
@@ -9,7 +13,10 @@ import org.springframework.stereotype.Component
 import java.io.IOException
 
 @Component
-class CustomAuthenticationSuccessHandler : AuthenticationSuccessHandler {
+class CustomAuthenticationSuccessHandler(
+    @Autowired
+    private val userRepository: UserRepository
+) : AuthenticationSuccessHandler {
     @Throws(IOException::class)
     override fun onAuthenticationSuccess(
         request: HttpServletRequest,
@@ -17,6 +24,17 @@ class CustomAuthenticationSuccessHandler : AuthenticationSuccessHandler {
         authentication: Authentication
     ) {
         val oidcUser = authentication.principal as OidcUser
+        val userEmail = oidcUser.email
+        val user: WorkoutCreativeUser? = userRepository.findByEmail(userEmail)
+
+        if (user == null) {
+            val newUser = WorkoutCreativeUser()
+            newUser.email = userEmail
+            newUser.name = oidcUser.fullName
+            newUser.profilePicture = oidcUser.picture
+            userRepository.save(newUser)
+        }
+
         val token = oidcUser.idToken.tokenValue
         response.setHeader("Access-Control-Allow-Credentials", "true")
 
