628d519b98de9cb29e39d53579fb582ce63cb490
Luke Williams <lukewilliams10101@gmail.com>
2024-02-01 21:55:19 +0100

    test: added test for status endpoint

--- backend/src/test/java/com/example/backend/status/StatusControllerTest.kt
+++ backend/src/test/java/com/example/backend/status/StatusControllerTest.kt
@@ -1,12 +1,36 @@
 package com.example.backend.status
 
 import org.junit.jupiter.api.Test
-
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
+
+@ExtendWith(SpringExtension::class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class StatusControllerTest {
 
+    @Value("\${server.port}")
+    private val port = 0
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
     @Test
-    fun statusCheck() {
+    fun shouldReturnServerStatus() {
+        val uri = "http://localhost:$port/api/status"
+        val expectedResponse = "Server is running"
+
+        mockMvc.perform(MockMvcRequestBuilders.get(uri))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.content().string(expectedResponse))
     }
 }
