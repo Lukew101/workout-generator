b74e84249dc03fb36f890c4f99be3e44d41d5a93
Luke Williams <lukewilliams10101@gmail.com>
2024-02-01 13:42:43 +0100

    feat: UserService injected into controller

--- backend/src/main/java/com/example/backend/user/controller/UserController.kt
+++ backend/src/main/java/com/example/backend/user/controller/UserController.kt
@@ -1,20 +1,28 @@
 package com.example.backend.user.controller
 
+import com.example.backend.user.controller.dtos.UserResponseData
+import com.example.backend.user.service.UserService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.core.oidc.user.OidcUser
 import org.springframework.web.bind.annotation.CrossOrigin
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
 @RequestMapping("api/user")
-class UserController {
+class UserController(
+    @Autowired
+    private val userService: UserService
+) {
+
     @GetMapping
-    fun getUser(@AuthenticationPrincipal user: OidcUser?): String {
-        if (user == null) return "No user"
-        return user.email.toString()
+    fun getUser(@AuthenticationPrincipal user: OidcUser?): UserResponseData? {
+        return userService.getUserById(user)
     }
+
 }
