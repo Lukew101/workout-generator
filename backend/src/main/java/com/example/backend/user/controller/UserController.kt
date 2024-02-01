207454c01b380456c44d57d4e147ffaa39a28429
Luke Williams <lukewilliams10101@gmail.com>
2024-02-01 21:40:39 +0100

    feat: implemented createProgram

--- backend/src/main/java/com/example/backend/user/controller/UserController.kt
+++ backend/src/main/java/com/example/backend/user/controller/UserController.kt
@@ -1,6 +1,9 @@
 package com.example.backend.user.controller
 
+import com.example.backend.exercise_generator.model.Exercise
+import com.example.backend.user.controller.dtos.ProgramRequestDTO
 import com.example.backend.user.controller.dtos.UserResponseData
+import com.example.backend.user.model.Program
 import com.example.backend.user.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.annotation.AuthenticationPrincipal
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -23,5 +27,10 @@ class UserController(
     @GetMapping
     fun getUser(@AuthenticationPrincipal user: OidcUser?): UserResponseData {
         return userService.getUserByEmail(user)
+    }
+
+    @PostMapping("/program")
+    fun createProgram(@AuthenticationPrincipal user: OidcUser, @RequestBody programData: ProgramRequestDTO): Program {
+        return userService.createProgram(programData, user)
     }
 }
