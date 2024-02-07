527a94feb9eaf84632ffeb2e016bb19a3676b346
Luke Williams <lukewilliams10101@gmail.com>
2024-02-07 16:40:23 +0100

    feat: now uses id token to set user after inital log in

--- client/src/app/myworkouts/page.tsx
+++ client/src/app/myworkouts/page.tsx
@@ -2,13 +2,14 @@
 import { useEffect, useState } from "react";
 import { User } from "../types";
 import { useCookies } from "react-cookie";
+import { jwtDecode } from "jwt-decode";
 
 export default function ProfileClient() {
   const [cookies, setCookie, removeCookie] = useCookies(["JwtToken"]);
   const [user, setUser] = useState<User>();
   const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
-  const fetchUserDetails = async () => {
+  const fetchUser= async () => {
     const cookieExpiration = new Date();
     cookieExpiration.setHours(cookieExpiration.getHours() + 8);
 
@@ -30,14 +31,25 @@ export default function ProfileClient() {
   };
 
   useEffect(() => {
-    fetchUserDetails();
+    if (!cookies.JwtToken) {
+      fetchUser();
+    }
+    
   }, []);
+
+  useEffect(() => {
+    if (cookies.JwtToken && user == null) {
+      const userToken: string = cookies.JwtToken;
+      const user: User = jwtDecode(userToken);
+      setUser(user);
+    }
+  }, [cookies.JwtToken]);
 
   return user ? (
     <div>
       <h2 className="mt-20">{user.name}</h2>
       <h2>{user.email}</h2>
-      <img src={user.profilePicture} alt={`${user.name} profile picture`} />
+      <img src={user.picture} alt={`${user.name} profile picture`} />
     </div>
   ) : (
     <h2>Please sign in</h2>
