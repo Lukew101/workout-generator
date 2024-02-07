48ce7608e245eb14980042c29a8b29278fcdf789
Luke Williams <lukewilliams10101@gmail.com>
2024-02-07 12:23:10 +0100

    fix: user does not fetch on home page

--- client/src/app/myworkouts/page.tsx
+++ client/src/app/myworkouts/page.tsx
@@ -1,30 +1,45 @@
 "use client";
 import { useEffect, useState } from "react";
 import { User } from "../types";
+import { useCookies } from "react-cookie";
 
 export default function ProfileClient() {
+  const [cookies, setCookie, removeCookie] = useCookies(["JwtToken"]);
   const [user, setUser] = useState<User>();
   const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
   const fetchUserDetails = async () => {
+    const cookieExpiration = new Date();
+    cookieExpiration.setHours(cookieExpiration.getHours() + 8);
+
     const res = await fetch(`${BACKEND_URL}/user`, {
       credentials: "include",
     });
+
+    const jwtToken = res.headers.get("IdToken");
+
+    setCookie("JwtToken", jwtToken, {
+      path: "/",
+      sameSite: "none",
+      secure: true,
+      expires: cookieExpiration,
+    });
+
     const data = await res.json();
     setUser(data);
-  }
+  };
 
   useEffect(() => {
     fetchUserDetails();
-  }, [])
+  }, []);
 
-  return (
-    user && (
-      <div>
-        <h2 className="mt-20">{user.name}</h2>
-        <h2>{user.email}</h2>
-        <img src={user.profilePicture} alt={`${user.name} profile picture`} />
-      </div>
-    )
+  return user ? (
+    <div>
+      <h2 className="mt-20">{user.name}</h2>
+      <h2>{user.email}</h2>
+      <img src={user.profilePicture} alt={`${user.name} profile picture`} />
+    </div>
+  ) : (
+    <h2>Please sign in</h2>
   );
 }
