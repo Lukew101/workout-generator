08d1cb2d994cf1798d362106ef5a8312f06d84cc
Luke Williams <lukewilliams10101@gmail.com>
2024-02-09 18:34:46 +0100

    feat: implemented program request dto

--- backend/src/main/java/com/example/backend/user/controller/dtos/ProgramRequestDTO.java
+++ backend/src/main/java/com/example/backend/user/controller/dtos/ProgramRequestDTO.java
@@ -1,8 +1,13 @@
 package com.example.backend.user.controller.dtos;
 
-import com.example.backend.exercise_generator.model.Exercise;
+import com.example.backend.exercise_generator.model.*;
 
 import java.util.List;
 
-public record ProgramRequestDTO(String name, List<Exercise> exerciseList) {
+public record ProgramRequestDTO(
+        String name,
+        List<StrengthExercise> strengthExercises,
+        List<CardioExercise> cardioExercises,
+        List<PlyometricExercise> plyometricExercises,
+        List<StretchingExercise> stretchingExercises) {
 }
